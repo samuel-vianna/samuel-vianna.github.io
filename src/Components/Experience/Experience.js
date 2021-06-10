cc3aea64835dc0e258adb14b4f56989f063b4f46
samuel-vianna <samuel-vianna@hotmail.com>
2021-06-10 16:51:03 -0300

    exp

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 import './exp_styles.scss';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import expList from './experiences.json';
 
 export default function Experience() {
 
@@ -16,18 +17,28 @@ export default function Experience() {
             <Container fluid className='page_container_no_limit' id='home_container_3'>
                 <Col id='row_container_3'>
                     <Row md='12' className='title_row'>Experience</Row>
-                    <Row md='12' >(hover the cards)</Row>
                     <div id='exp_container'>
                         <div id='wrap'></div>
-                        <div className='exp_year'>
-                            <h5>2018</h5>
-                            <div className='year_info'>
-                                <ExperienceCard />
-                                <ExperienceCard />
-                                <ExperienceCard />
-                                <ExperienceCard />
-                            </div>
+                        <div>
+                            {expList.map(exp => {
+                                return (
+                                    <div className='exp_year'>
+                                        <h5>{exp.year}</h5>
+                                        <div className='year_info'>
+                                            {exp.list.map(card => {
+                                                return (
+                                                    <ExperienceCard info={card} />
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                )
+                            })
+                            }
+
                         </div>
+
+
                     </div>
                 </Col>
 
@@ -39,10 +50,24 @@ export default function Experience() {
 
 
 export function ExperienceCard(props) {
+    var info = props.info
 
     return (
         <div className='card_exp'>
-            <h5 id='card_title'>Tools</h5>
+            <h5 id='card_title'>{info.title}</h5>
+            <div className='date-row'>
+                <div className='date-info'>
+                    <p className='date-desc'>Start:</p>
+                    <p className='date-number'>{info.start}</p>
+                </div>
+
+                <div className='date-info'>
+                    <p className='date-desc'>End:</p>
+                    <p className='date-number'>{info.end}</p>
+                </div>
+            </div>
+
+            <p>{info.description}</p>
 
         </div>
     )
